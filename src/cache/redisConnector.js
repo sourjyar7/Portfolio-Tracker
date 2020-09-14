cbccb8167a81c2bc659fa0c479a1c30661469cce
sourjyar7 <sourjyar7@gmail.com>
2020-09-15 02:54:06 +0530

    added remote redis support

--- src/cache/redisConnector.js
+++ src/cache/redisConnector.js
@@ -3,7 +3,10 @@ const redis=require('redis');
 const redisConnect=()=>{
 
 
-const redisClient=redis.createClient(process.env.REDIS_PORT);
+const redisClient=redis.createClient({port:process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST,
+    password: process.env.REDIS_PASSWORD
+});
 console.log("connected to redis");
 return redisClient;
 
